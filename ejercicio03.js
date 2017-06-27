fda093fd63136ef0bbb1e5111ac0261e731e4d01
gabiprds93 <gabiprds.93@gmail.com>
2017-06-27 09:44:01 -0500

    Ejercicio 3 mejorado

--- ejercicio03.js
+++ ejercicio03.js
@@ -1,30 +1,25 @@
 function ejercicio3(arreglo)
 {
-    var texto = {
-        propiedad1: arreglo[0],
-        propiedad2: arreglo[1],
-        propiedad3: arreglo[2],
-        propiedad4: arreglo[3],
-        propiedad5: arreglo[4],
-        propiedad6: arreglo[5],
-    }
-    
-    function cadena()
+    var texto = new Object();
+    for(var i = 1; i <= arreglo.length; i++)
     {
-        var propiedades = Object.keys(texto);
-        var resultado = "";
-        for(var i = 0; i < arreglo.length; i++)
-        {
-            resultado += propiedades[i] + "-->" + arreglo[i] + ";";
-        }
-        resultado = resultado.slice(0, -1);
-        return resultado;
+        texto["propiedad" + i] = arreglo[i-1];
     }
-    return cadena();
+    var propiedades = Object.keys(texto);
+    var resultado = "";
+    for(var i = 0; i < arreglo.length; i++)
+    {
+        resultado += propiedades[i] + "-->" + arreglo[i] + ";";
+    }
+    resultado = resultado.slice(0, -1);
+    return resultado;
 }
 var assert = require("assert");
 describe("Prueba ejercicio 3", function(){
     it("Pruebas con arreglo[6, 5, 4, 3, 2, 1]", function(){
         assert.equal("propiedad1-->6;propiedad2-->5;propiedad3-->4;propiedad4-->3;propiedad5-->2;propiedad6-->1", ejercicio3([6, 5, 4, 3, 2, 1]));
+    });
+    it("Pruebas con arreglo[6, 5, 4, 3, 2, 1]", function(){
+        assert.equal("propiedad1-->7;propiedad2-->6;propiedad3-->5;propiedad4-->4;propiedad5-->3;propiedad6-->2;propiedad7-->1", ejercicio3([7, 6, 5, 4, 3, 2, 1]));
     });
 });
